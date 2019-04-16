72e8f57fed547564257a25a07f07a276eb20f708
Kush <kushthedude@gmail.com>
2019-04-16 10:03:11 +0530

    Amazing Prime Sequence

---
+++ APS.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+ 
+typedef long long L;
+ 
+L factor[10000001],a[10000001];
+ 
+int main(){
+    L i,j;
+    for(i=2;i<=10000000;i++){
+        if(factor[i]==0){
+            factor[i] = i;
+            for(j=i*i;j<=10000000;j+=i){
+                if(factor[j]==0) factor[j] = i;
+            }
+        }
+        a[i] = a[i-1] + factor[i];
+    }
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%lld",&i);
+        printf("%lld\n",a[i]);
+    }
+    return 0;
+}
